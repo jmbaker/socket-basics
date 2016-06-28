5c79cd8b830dfd497009d0b8bbeed93e8ad2f332
Jeremy <user.email>
2016-06-27 19:31:06 -0500

    Add name support

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,8 @@
 var socket = io();
+var name = getQueryVariable('name') || 'Anonymous';
+var room = getQueryVariable('room');
+
+console.log(name + ' wants to join ' + room);
 
 socket.on('connect', function() {
 	console.log('Connected to socket.io server!');
@@ -9,9 +13,10 @@ socket.on('message', function(message) {
 	console.log('New message: ');
 	console.log(message.text);
 
+	var $message = jQuery('.messages');
 
-	jQuery('.messages').append('<p><strong>' + momentTimestamp.local().format('h:mm a') + ': </strong>'
-	  + message.text + '</p>');
+	$message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('h:mm a') + ':  </p></strong>');
+	$message.append('<p>' + message.text + '</p>');
 });
 
 // Handles submitting of new message
@@ -23,6 +28,7 @@ $form.on('submit', function(event) {
 	var $message = $form.find('input[name=message]');
 
 	socket.emit('message', {
+		name: name,
 		text: $message.val()
 	})
 
